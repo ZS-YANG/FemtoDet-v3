c2d85607ab8b74f74384e90f46243831fd793a37
ZS-YANG <1677926753@qq.com>
2023-12-12 02:32:46 +0800

    update init

--- README.md
+++ README.md
@@ -1,1 +1,80 @@
 # FemtoDet-v3
+
+[FemtoDet](https://github.com/yh-pengtu/FemtoDet)的非官方实现
+
+paper:[Femtodet: an object detection baseline for energy versus performance tradeoffs](https://arxiv.org/abs/2301.06719)
+
+## 写在前面
+
+原作者代码基于 mmdet2.x，由于mmdet3.x进行了大量更新，原始代码不能直接应用于mmdet3.0的项目中，因此基于mmdet3.2.0对原始代码进行了重构
+初始代码基于
+
+## To list
+
+- [ ] 发布在VOC0712数据集上的训练结果
+- [ ] 对代码进行进一步重构解耦
+- [ ] 更新集教程文档
+- [ ] 精度对齐(使用[femtodet_0stage_yolox.py](configs%2FfemtoDet%2Ffemtodet_0stage_yolox.py)
+  的训练结果比[femtodet_0stage.py](configs%2FfemtoDet%2Ffemtodet_0stage.py)高很多，需要找到原因)
+
+### Dependencies and Installation
+
+按照[mmdetection](https://github.com/open-mmlab/mmdetection/tree/v3.2.0)的要求安装torch 和 mmcv
+
+之后克隆本项目，进行mmdet的安装
+
+```commandline
+git clone https://github.com/ZS-YANG/FemtoDet-v3.git
+cd FemtoDet-v3
+pip install .
+```
+
+### Preparation
+
+1. Download the dataset.
+
+   We mainly train FemtoDet on [Pascal VOC 0712](http://host.robots.ox.ac.uk/pascal/VOC/), you should firstly download
+   the datasets. By default, we assume the dataset is stored in ./data/.
+
+2. Dataset preparation.
+   使用[pascal_voc.py](tools%2Fdataset_converters%2Fpascal_voc.py)
+   转换数据集格式，转换完成后配置[femtoDet](configs%2FfemtoDet)中的数据集路径
+
+3. Download the initialized models.
+
+   We trained our designed backbone on ImageNet 1k, and used it
+   for [the inite weights](https://pan.baidu.com/s/1JGsvlvzPkb5nxGBaRSD7ng?pwd=hx8k))(hx8k) of FemtoDet.
+
+### Results (trained on VOC) and Models
+
+原始作者训练精度
+
+| Detector | Params | box AP50 | Config                                |
+|----------|--------|----------|---------------------------------------|
+|          |        | 37.1     | ./configs/femtoDet/femtodet_0stage.py |
+| FemtoDet | 68.77k | 40.4     | ./configs/femtoDet/femtodet_1stage.py |
+|          |        | 44.4     | ./configs/femtoDet/femtodet_2stage.py |
+|          |        | 46.5     | ./configs/femtoDet/femtodet_3stage.py |
+
+### References
+
+If you find the code useful for your research, please consider citing:
+
+```bib
+@InProceedings{Tu_2023_ICCV,
+    author    = {Tu, Peng and Xie, Xu and Ai, Guo and Li, Yuexiang and Huang, Yawen and Zheng, Yefeng},
+    title     = {FemtoDet: An Object Detection Baseline for Energy Versus Performance Tradeoffs},
+    booktitle = {Proceedings of the IEEE/CVF International Conference on Computer Vision (ICCV)},
+    month     = {October},
+    year      = {2023},
+    pages     = {13318-13327}
+}
+@misc{tu2023femtodet,
+      title={FemtoDet: An Object Detection Baseline for Energy Versus Performance Tradeoffs}, 
+      author={Peng Tu and Xu Xie and Guo AI and Yuexiang Li and Yawen Huang and Yefeng Zheng},
+      year={2023},
+      eprint={2301.06719},
+      archivePrefix={arXiv},
+      primaryClass={cs.CV}
+}
+```
---
+++ configs/femtoDet/femtodet_0stage_yolox.py
@@ -0,0 +1,265 @@
+_base_ = [
+    '../_base_/schedules/schedule_1x.py',
+    '../_base_/default_runtime.py',
+    # './yolox_tta.py'
+]
+classes = ('aeroplane', 'bicycle', 'bird', 'boat', 'bottle', 'bus', 'car', 'cat', 'chair', 'cow', 'diningtable',
+           'dog', 'horse', 'motorbike', 'person', 'pottedplant', 'sheep', 'sofa', 'train', 'tvmonitor')
+
+img_scale = (640, 640)  # width, height
+test_img_scale = (416, 416)
+
+# model settings
+widen_factor = 0.25
+pretrain_weight = './weights/femtodet_nano_backbone_pretrained_model_witc_cd_conv2_4973.pth'
+load_from = None
+resume_from = None
+default_channels = [32, 96, 320]
+neck_in_chanels = [int(ch * widen_factor) for ch in default_channels]
+headfeat_channel = 64
+
+model = dict(
+    type='FemtoDet',
+    data_preprocessor=dict(
+        type='DetDataPreprocessor',
+        pad_size_divisor=32,
+        batch_augments=[
+            dict(
+                type='BatchSyncRandomResize',
+                random_size_range=(480, 800),
+                size_divisor=32,
+                interval=10)
+        ]),
+    backbone=dict(
+        type='FemtoNet',
+        widen_factor=widen_factor,
+        diff_conv=True,
+        out_indices=(2, 4, 6),
+        act_cfg=dict(type='ReLU'),
+        init_cfg=dict(
+            type='Pretrained',
+            prefix='backbone',
+            checkpoint=pretrain_weight)),
+    neck=dict(
+        type='SharedNeck',
+        in_channels=neck_in_chanels,
+        out_channels=headfeat_channel,
+        fixed_size_idx=1,
+        add=True,
+        norm_cfg=dict(type='BN'),
+        act_cfg=dict(type='ReLU'),
+        num_outs=1),
+    bbox_head=dict(
+        type='YOLOXHead',
+        num_classes=len(classes),
+        in_channels=headfeat_channel,
+        feat_channels=headfeat_channel,
+        strides=[16, ],
+        stacked_convs=0,
+        loss_bbox=dict(
+            type='IoULoss',
+            mode='square',
+            eps=1e-16,
+            reduction='sum',
+            loss_weight=5.0),
+        act_cfg=dict(type='ReLU'),
+        use_depthwise=True),
+    train_cfg=dict(assigner=dict(type='SimOTAAssigner', center_radius=2.5)),
+    # In order to align the source code, the threshold of the val phase is
+    # 0.01, and the threshold of the test phase is 0.001.
+    test_cfg=dict(score_thr=0.01, nms=dict(type='nms', iou_threshold=0.65)))
+
+# dataset settings
+data_root = '/media/yzs/ubuntu/datasets/VOC'
+dataset_type = 'CocoDataset'
+img_norm_cfg = dict(mean=[123.675, 116.28, 103.53], std=[58.82, 58.82, 58.82], to_rgb=True)
+
+# Example to use different file client
+# Method 1: simply set the data root and let the file I/O module
+# automatically infer from prefix (not support LMDB and Memcache yet)
+
+# data_root = 's3://openmmlab/datasets/detection/coco/'
+
+# Method 2: Use `backend_args`, `file_client_args` in versions before 3.0.0rc6
+# backend_args = dict(
+#     backend='petrel',
+#     path_mapping=dict({
+#         './data/': 's3://openmmlab/datasets/detection/',
+#         'data/': 's3://openmmlab/datasets/detection/'
+#     }))
+backend_args = None
+
+train_pipeline = [
+    dict(type='Mosaic', img_scale=img_scale, pad_val=114.0, prob=0.5),
+    dict(
+        type='RandomAffine',
+        superposition=False,
+        scaling_ratio_range=(0.5, 1.5),
+        # img_scale is (width, height)
+        border=(-img_scale[0] // 2, -img_scale[1] // 2)),
+    dict(
+        type='FemtoDetMixUp',
+        superposition=False,
+        img_scale=img_scale,
+        ratio_range=(0.8, 1.2),
+        pad_val=114.0),
+    dict(type='YOLOXHSVRandomAug_EodVersion', hgain=0.015, sgain=0.7, vgain=0.4),
+    dict(type='RandomFlip', prob=0.5),
+    # According to the official implementation, multi-scale
+    # training is not considered here but in the
+    # 'mmdet/models/detectors/yolox.py'.
+    # Resize and Pad are for the last 15 epochs when Mosaic,
+    # RandomAffine, and MixUp are closed by YOLOXModeSwitchHook.
+    dict(type='Resize', scale=img_scale, keep_ratio=True),
+    dict(
+        type='Pad',
+        pad_to_square=True,
+        # If the image is three-channel, the pad value needs
+        # to be set separately for each channel.
+        pad_val=dict(img=(114.0, 114.0, 114.0))),
+    dict(type='Normalize', **img_norm_cfg),
+    dict(type='FilterAnnotations', min_gt_bbox_wh=(1, 1), keep_empty=False),
+    dict(type='PackDetInputs')
+]
+
+train_dataset = dict(
+    # use MultiImageMixDataset wrapper to support mosaic and mixup
+    type='MultiImageMixDataset',
+    dataset=dict(
+        type=dataset_type,
+        data_root=data_root,
+        ann_file='/media/yzs/ubuntu/datasets/VOC/coco/voc0712_trainval.json',
+        data_prefix=dict(img='VOCdevkit/'),
+        pipeline=[
+            dict(type='LoadImageFromFile', backend_args=backend_args),
+            dict(type='LoadAnnotations', with_bbox=True)
+        ],
+        filter_cfg=dict(filter_empty_gt=False, min_size=32),
+        backend_args=backend_args),
+    pipeline=train_pipeline)
+
+test_pipeline = [
+    dict(type='LoadImageFromFile', backend_args=backend_args),
+    dict(type='Resize', scale=test_img_scale, keep_ratio=True),
+    dict(
+        type='Pad',
+        pad_to_square=True,
+        pad_val=dict(img=(114.0, 114.0, 114.0))),
+    dict(type='Normalize', **img_norm_cfg),
+    dict(type='LoadAnnotations', with_bbox=True),
+    dict(
+        type='PackDetInputs',
+        meta_keys=('img_id', 'img_path', 'ori_shape', 'img_shape',
+                   'scale_factor'))
+]
+
+train_dataloader = dict(
+    batch_size=32,
+    num_workers=8,
+    persistent_workers=True,
+    sampler=dict(type='DefaultSampler', shuffle=True),
+    dataset=train_dataset)
+val_dataloader = dict(
+    batch_size=8,
+    num_workers=4,
+    persistent_workers=True,
+    drop_last=False,
+    sampler=dict(type='DefaultSampler', shuffle=False),
+    dataset=dict(
+        type=dataset_type,
+        data_root=data_root,
+        ann_file='/media/yzs/ubuntu/datasets/VOC/coco/voc07_test.json',
+        data_prefix=dict(img='VOCdevkit'),
+        test_mode=True,
+        pipeline=test_pipeline,
+        backend_args=backend_args))
+test_dataloader = val_dataloader
+
+val_evaluator = dict(
+    type='CocoMetric',
+    # ann_file=data_root + 'annotations/instances_val2017.json',
+    ann_file='/media/yzs/ubuntu/datasets/VOC/coco/voc07_test.json',
+    metric='bbox',
+    backend_args=backend_args)
+
+test_evaluator = val_evaluator
+
+# training settings
+max_epochs = 300
+num_last_epochs = 15
+interval = 10
+
+train_cfg = dict(max_epochs=max_epochs, val_interval=interval)
+
+# optimizer
+# default 8 gpu
+base_lr = 0.01
+optim_wrapper = dict(
+    type='OptimWrapper',
+    optimizer=dict(
+        type='SGD', lr=base_lr, momentum=0.9, weight_decay=5e-4,
+        nesterov=True),
+    paramwise_cfg=dict(norm_decay_mult=0., bias_decay_mult=0.))
+
+# learning rate
+param_scheduler = [
+    dict(
+        # use quadratic formula to warm up 5 epochs
+        # and lr is updated by iteration
+        # TODO: fix default scope in get function
+        type='mmdet.QuadraticWarmupLR',
+        by_epoch=True,
+        begin=0,
+        end=5,
+        convert_to_iter_based=True),
+    dict(
+        # use cosine lr from 5 to 285 epoch
+        type='CosineAnnealingLR',
+        eta_min=base_lr * 0.05,
+        begin=5,
+        T_max=max_epochs - num_last_epochs,
+        end=max_epochs - num_last_epochs,
+        by_epoch=True,
+        convert_to_iter_based=True),
+    dict(
+        # use fixed lr during last 15 epochs
+        type='ConstantLR',
+        by_epoch=True,
+        factor=1,
+        begin=max_epochs - num_last_epochs,
+        end=max_epochs,
+    )
+]
+
+default_hooks = dict(
+    checkpoint=dict(
+        interval=interval,
+        max_keep_ckpts=3  # only keep latest 3 checkpoints
+    ))
+
+custom_hooks = [
+    dict(
+        type='FemtoDetModeSwitchHook',
+        num_last_epochs=num_last_epochs,
+        priority=48,
+        all_unfreeze_after_this_epoch=50
+    ),
+    dict(type='SyncNormHook', priority=48),
+    dict(
+        type='EMAHook',
+        ema_type='ExpMomentumEMA',
+        momentum=0.0001,
+        update_buffers=True,
+        priority=49)
+]
+find_unused_parameters = True
+dist_params = dict(backend='nccl')
+log_level = 'INFO'
+workflow = [('train', 1)]
+opencv_num_threads = 0
+mp_start_method = 'fork'
+
+# NOTE: `auto_scale_lr` is for automatically scaling LR,
+# USER SHOULD NOT CHANGE ITS VALUES.
+# base_batch_size = (8 GPUs) x (8 samples per GPU)
+# auto_scale_lr = dict(base_batch_size=64)
